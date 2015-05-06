ec244b458c47483cd1a089b8521f9d1baed358d9
rassol <rassol@karmeye.com>
2015-05-06 16:24:50 +0300

    Doc

--- Source/KMYDraggableCollectionViewDataSource.h
+++ Source/KMYDraggableCollectionViewDataSource.h
@@ -10,12 +10,18 @@
 
 @required
 
-- (void)collectionView:(UICollectionView *)collectionView moveItemAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath;
+/// @return YES to allow the user to begin to move the item at @c indexPath.
 - (BOOL)collectionView:(UICollectionView *)collectionView canMoveItemAtIndexPath:(NSIndexPath *)indexPath;
+
+/// Called when the user confirmes the move and the animation will start. Change the datasource order here.
+- (void)collectionView:(UICollectionView *)collectionView moveItemAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath;
 
 @optional
 
+/// @return YES to allow the user to move the item currently at @c indexPath to @c toIndexPath.
 - (BOOL)collectionView:(UICollectionView *)collectionView canMoveItemAtIndexPath:(NSIndexPath *)indexPath toIndexPath:(NSIndexPath *)toIndexPath;
+
+/// Called after @c moveItemAtIndexPath:toIndexPath: and after the animation has finished.
 - (void)collectionView:(UICollectionView *)collectionView didMoveItemAtIndexPath:(NSIndexPath *)indexPath toIndexPath:(NSIndexPath *)toIndexPath;
 
 @end
